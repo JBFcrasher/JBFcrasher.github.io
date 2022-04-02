d4a8c82f35dad4cb1efd3bedb4b857e11b47c3c2
JBFcrasher <82116224+JBFcrasher@users.noreply.github.com>
2022-04-02 16:37:44 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,38 +5,40 @@ function applycss(){
     let nidttp1r ="JBFcrasher";
     let time="1:23.45";
     
-    document.getElementById("64bftp1").classList.add('AxlThunder');
-    document.getElementById("64bfli1").classList.add('AxlThunder');
-    document.getElementById("64bfac1").classList.add('AxlThunder');
-    document.getElementById("64bfgi1").classList.add('AxlThunder');
-    document.getElementById("64bflp1").classList.add('AxlThunder');
-    document.getElementById("64bffe1").classList.add('AxlThunder');
-    document.getElementById("64bfsg1").classList.add('AxlThunder');
-    document.getElementById("64bfvc1").classList.add('AxlThunder');
-    document.getElementById("64bfny1").classList.add('AxlThunder');
-    document.getElementById("64bfcc1").classList.add('AxlThunder');
-    document.getElementById("64bfhs1").classList.add('AxlThunder');
-    document.getElementById("64bfna1").classList.add('AxlThunder');
+    document.getElementById("nibftp1").classList.add('AxlThunder');
+    document.getElementById("nibfli1").classList.add('AxlThunder');
+    document.getElementById("nibfac1").classList.add('AxlThunder');
+    document.getElementById("nibfgi1").classList.add('AxlThunder');
+    document.getElementById("nibflp1").classList.add('AxlThunder');
+    document.getElementById("nibffe1").classList.add('AxlThunder');
+    document.getElementById("nibfsg1").classList.add('AxlThunder');
+    document.getElementById("nibfvc1").classList.add('AxlThunder');
+    document.getElementById("nibfny1").classList.add('AxlThunder');
+    document.getElementById("nibfcc1").classList.add('AxlThunder');
+    document.getElementById("nibfhs1").classList.add('AxlThunder');
+    document.getElementById("nibfna1").classList.add('AxlThunder');
 
     
-    document.getElementById("64tbli1").classList.add('Lazy');
-    document.getElementById("64tbac1").classList.add('Lazy');
-    document.getElementById("64tbgi1").classList.add('Lazy');
-    document.getElementById("64tbfe1").classList.add('Lazy');
-    document.getElementById("64tbna1").classList.add('Lazy');
+    document.getElementById("nitbli1").classList.add('Lazy');
+    document.getElementById("nitbac1").classList.add('Lazy');
+    document.getElementById("nitbgi1").classList.add('Lazy');
+    document.getElementById("nitbfe1").classList.add('Lazy');
+    document.getElementById("nitbna1").classList.add('Lazy');
 
 
-    document.getElementById("64ctgi1").classList.add('Inphlox');
-    document.getElementById("64rhgi1").classList.add('Inphlox');
-    document.getElementById("64bfcd1").classList.add('Inphlox');
+    document.getElementById("nictgi1").classList.add('Inphlox');
+    document.getElementById("nirhgi1").classList.add('Inphlox');
+    document.getElementById("nibfcd1").classList.add('Inphlox');
     
     
  
-   document.getElementById("64rhfe1").classList.add('Gamedraco');
-    document.getElementById("64rhac1").classList.add('Gamedraco');
+   document.getElementById("nirhfe1").classList.add('Gamedraco');
+    document.getElementById("nirhac1").classList.add('Gamedraco');
 
     
 
-    //document.getElementById("64dttp1").classList.add('JBFcrasher'); document.GetElementById("64dttp1l").innerHTML = "1:12.29";
-  document.getElementById("64dttp1l").textContent = time; document.getElementById("64dttp1").classList.add(nidttp1r);
+    //document.getElementById("nidttp1").classList.add('JBFcrasher'); document.GetElementById("nidttp1l").innerHTML = "1:12.29";
+  document.getElementById("nidttp1l").textContent = time; document.getElementById("nidttp1").classList.add(nidttp1r);
 }
+
+
