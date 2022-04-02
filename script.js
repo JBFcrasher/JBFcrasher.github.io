69b6ea603da20f22e354765d144b082d8a65fa40
JBFcrasher <82116224+JBFcrasher@users.noreply.github.com>
2022-04-02 16:49:57 -0700

    Update script.js

--- script.js
+++ script.js
@@ -33,8 +33,8 @@ function applycss(){
     
     
  
-   document.getElementById("nirhfe1").classList.add('nirhfe1r');
-    document.getElementById("nirhac1").classList.add('nirhac1r');
+   document.getElementById("nirhfe1").classList.add(nirhfe1r);
+    document.getElementById("nirhac1").classList.add(nirhac1r);
 
     
 
