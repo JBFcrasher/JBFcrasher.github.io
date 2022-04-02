d0c54414e9a9ac1b41ba9f095277b21f87f2c4a8
JBFcrasher <82116224+JBFcrasher@users.noreply.github.com>
2022-04-02 12:59:36 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,35 @@
 setTimeout(function(){
     document.getElementById("64bftp1").classList.add('AxlThunder');
-    document.getElementById("64bftp1").classList.add('AxlThunder');
+    document.getElementById("64bfli1").classList.add('AxlThunder');
+    document.getElementById("64bfac1").classList.add('AxlThunder');
+    document.getElementById("64bfgi1").classList.add('AxlThunder');
+    document.getElementById("64bflp1").classList.add('AxlThunder');
+    document.getElementById("64bffe1").classList.add('AxlThunder');
+    document.getElementById("64bfsg1").classList.add('AxlThunder');
+    document.getElementById("64bfvc1").classList.add('AxlThunder');
+    document.getElementById("64bfny1").classList.add('AxlThunder');
+    document.getElementById("64bfcc1").classList.add('AxlThunder');
+    document.getElementById("64bfhs1").classList.add('AxlThunder');
+    document.getElementById("64bfna1").classList.add('AxlThunder');
+
+    
+    document.getElementById("64tbli1").classList.add('Lazy');
+    document.getElementById("64tbac1").classList.add('Lazy');
+    document.getElementById("64tbgi1").classList.add('Lazy');
+    document.getElementById("64tbfe1").classList.add('Lazy');
+    document.getElementById("64tbna1").classList.add('Lazy');
+
+
+    document.getElementById("64ctgi1").classList.add('Inphlox');
+    document.getElementById("64rhgi1").classList.add('Inphlox');
+    document.getElementById("64bfcd1").classList.add('Inphlox');
+
+
+
+
+
+
+
+
+
 }, 500);
