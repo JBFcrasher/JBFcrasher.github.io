307ce5dc6150a399b7664cac7b66093d4326bd4d
JBFcrasher <82116224+JBFcrasher@users.noreply.github.com>
2022-04-02 15:42:46 -0700

    Update script.js

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ function applycss(){
         var scrt_var = 10;
       var strLink = "2.html&Key=" + scrt_var;
 
-   document.getElementById("64dttp1").setAttribute("href",strLink); document.getElementById("64dttp1").classList.add('JBFcrasher');
+   document.getElementById("64dttp1l").setAttribute("href",strLink); document.getElementById("64dttp1").classList.add('JBFcrasher');
     
 
 
