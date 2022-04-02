1ac217df3bc0e160cd854f4aa41aed14d571fffd
JBFcrasher <82116224+JBFcrasher@users.noreply.github.com>
2022-04-02 12:39:05 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
 setTimeout(function(){
-    //Add the CSS class by using classList.add.
     document.getElementById("64rhgi").classList.add('Inphlox');
-    document.getElementById("64rhgi").classList.remove('times');
 }, 500);
