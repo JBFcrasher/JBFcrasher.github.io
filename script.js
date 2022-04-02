ed46cb339b37d7398701ce31c9c15d2fb08f0ed4
JBFcrasher <82116224+JBFcrasher@users.noreply.github.com>
2022-04-02 16:25:39 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 
 function applycss(){
-   // let 64dttp1r ="JBFcrasher";
+    let runner ="JBFcrasher";
     let time="1:23.45";
     
     document.getElementById("64bftp1").classList.add('AxlThunder');
@@ -38,5 +38,5 @@ function applycss(){
     
 
     //document.getElementById("64dttp1").classList.add('JBFcrasher'); document.GetElementById("64dttp1l").innerHTML = "1:12.29";
-  document.getElementById("64dttp1l").textContent = time; document.getElementById("64dttp1").classList.add("JBFcrasher");
+  document.getElementById("64dttp1l").textContent = time; document.getElementById("64dttp1").classList.add(runner);
 }
