86daf38529d57f02d7414ee1f44c8e18311720de
JBFcrasher <82116224+JBFcrasher@users.noreply.github.com>
2022-04-02 19:55:54 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,14 @@
 
 
 function applycss(){
-    let nidttp1r ="JBFcrasher"; let nidttp1t="test"; 
-    let nirhac1r="Gamedraco"; let nirhac1t="1:33.79";
-    let nirhfe1r="Gamedraco"; let nirhfe1t="1:25.43"; let nirhfe1a="https://www.youtube.com/watch?v=OKjYxLI12ak";
-    let nitbna1r	="Lazy Comet";	let nitbna1t	="3:15.76";	let nitbna1a	="https://www.twitch.tv/videos/1441988246";
+let nibfna1r	="Gamedraco";	let nibfna1t	="2:44.40";	let nibfna1a	="https://www.youtube.com/watch?v=xeFMb4eP05Y&t=1s";
+let nirhac1r	="Gamedraco";	let nirhac1t	="1:33.79";	let nirhac1a	="https://www.youtube.com/watch?v=nOOIJa7WsMo";
+let nirhfe1r	="Gamedraco";	let nirhfe1t	="1:25.43";	let nirhfe1a	="https://www.youtube.com/watch?v=OKjYxLI12ak";
+let nitbac1r	="Lazy Comet";	let nitbac1t	="1:50.32";	let nitbac1a	="https://www.twitch.tv/videos/1441988243";
+let nitbfe1r	="Lazy Comet";	let nitbfe1t	="1:45.79";	let nitbfe1a	="https://www.twitch.tv/videos/1441988249";
+let nitbgi1r	="Lazy Comet";	let nitbgi1t	="1:45.75";	let nitbgi1a	="https://www.twitch.tv/videos/1441988244";
+let nitbli1r	="Lazy Comet";	let nitbli1t	="1:46.06";	let nitbli1a	="https://www.twitch.tv/videos/1441988245";
+let nitbna1r	="Lazy Comet";	let nitbna1t	="3:15.76";	let nitbna1a	="https://www.twitch.tv/videos/1441988246";
     
     document.getElementById("nibftp1").classList.add('AxlThunder');
     document.getElementById("nibfli1").classList.add('AxlThunder');
@@ -40,9 +44,14 @@ function applycss(){
     
 //                          cell link id                  track time                       cell ID           track runner
 //  document.getElementById("nidttp1l").textContent = nidttp1t; document.getElementById("nidttp1").classList.add(nidttp1r);
-  document.getElementById("nidttp1l").textContent = nidttp1t; document.getElementById("nidttp1").classList.add(nidttp1r);
+
+  document.getElementById("nibfna1l").textContent = nibfna1t; document.getElementById("nibfna1").classList.add(nibfna1r);
   document.getElementById("nirhac1l").textContent = nirhac1t; document.getElementById("nirhac1").classList.add(nirhac1r);
   document.getElementById("nirhfe1l").textContent = nirhfe1t; document.getElementById("nirhfe1").classList.add(nirhfe1r);
+  document.getElementById("nitbac1l").textContent = nitbac1t; document.getElementById("nitbac1").classList.add(nitbac1r);
+  document.getElementById("nitbfe1l").textContent = nitbfe1t; document.getElementById("nitbfe1").classList.add(nitbfe1r);
+  document.getElementById("nitbgi1l").textContent = nitbgi1t; document.getElementById("nitbgi1").classList.add(nitbgi1r);
+  document.getElementById("nitbli1l").textContent = nitbli1t; document.getElementById("nitbli1").classList.add(nitbli1r);
   document.getElementById("nitbna1l").textContent = nitbna1t; document.getElementById("nitbna1").classList.add(nitbna1r);
 }
 
