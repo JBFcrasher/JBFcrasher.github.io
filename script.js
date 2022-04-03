49277d1b1e45311e1036cceafdba0077407a2d82
JBFcrasher <82116224+JBFcrasher@users.noreply.github.com>
2022-04-03 08:20:40 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
 //example time: JBFcrasher N64 Thunder Park Damn the Torpedoes 1:23.45
 
 
-function applycss(){
-let nibfna2r	="Gamedraco";	let nibfna2t	="2:44.40";	let nibfna2a	="https://www.youtube.com/watch?v=xeFMb4eP05Y&t=1s";
-let nirhac1r	="Gamedraco";	let nirhac1t	="1:33.79";	let nirhac1a	="https://www.youtube.com/watch?v=nOOIJa7WsMo";
-let nirhfe1r	="Gamedraco";	let nirhfe1t	="1:25.43";	let nirhfe1a	="https://www.youtube.com/watch?v=OKjYxLI12ak";
-let nitbac1r	="LazyComet";	let nitbac1t	="1:50.32";	let nitbac1a	="https://www.twitch.tv/videos/1441988243";
-let nitbfe1r	="LazyComet";	let nitbfe1t	="1:45.79";	let nitbfe1a	="https://www.twitch.tv/videos/1441988249";
-let nitbgi1r	="LazyComet";	let nitbgi1t	="1:45.75";	let nitbgi1a	="https://www.twitch.tv/videos/1441988244";
-let nitbli1r	="LazyComet";	let nitbli1t	="1:46.06";	let nitbli1a	="https://www.twitch.tv/videos/1441988245";
-let nitbna1r	="LazyComet";	let nitbna1t	="3:15.76";	let nitbna1a	="https://www.twitch.tv/videos/1441988246";
+function applycss(){let nibfna1r="Gamedraco"; let nibfna1t="2:44.40"; let nibfna1a="https://www.youtube.com/watch?v=xeFMb4eP05Y&t=1s";	document.getElementById("nibfna1l).textContent = nibfna1t;document.getElementById("nibfna1).textContent = nibfna1r;
+let nirhac1r="Gamedraco"; let nirhac1t="1:33.79"; let nirhac1a="https://www.youtube.com/watch?v=nOOIJa7WsMo";	document.getElementById("nirhac1l).textContent = nirhac1t;document.getElementById("nirhac1).textContent = nirhac1r;
+let nirhfe1r="Gamedraco"; let nirhfe1t="1:25.43"; let nirhfe1a="https://www.youtube.com/watch?v=OKjYxLI12ak";	document.getElementById("nirhfe1l).textContent = nirhfe1t;document.getElementById("nirhfe1).textContent = nirhfe1r;
+let nitbac1r="LazyComet"; let nitbac1t="1:50.32"; let nitbac1a="https://www.twitch.tv/videos/1441988243";	document.getElementById("nitbac1l).textContent = nitbac1t;document.getElementById("nitbac1).textContent = nitbac1r;
+let nitbfe1r="LazyComet"; let nitbfe1t="1:39.82"; let nitbfe1a="https://www.twitch.tv/videos/1445009096";	document.getElementById("nitbfe1l).textContent = nitbfe1t;document.getElementById("nitbfe1).textContent = nitbfe1r;
+//let nitbfe2r="LazyComet"; let nitbfe2t="1:45.79"; let nitbfe2a="https://www.twitch.tv/videos/1441988249";	document.getElementById("nitbfe2l).textContent = nitbfe2t;document.getElementById("nitbfe2).textContent = nitbfe2r;
+let nitbgi1r="LazyComet"; let nitbgi1t="1:41.85"; let nitbgi1a="https://www.twitch.tv/videos/1445009095";	document.getElementById("nitbgi1l).textContent = nitbgi1t;document.getElementById("nitbgi1).textContent = nitbgi1r;
+//let nitbgi2r="LazyComet"; let nitbgi2t="1:45.75"; let nitbgi2a="https://www.twitch.tv/videos/1441988244";	document.getElementById("nitbgi2l).textContent = nitbgi2t;document.getElementById("nitbgi2).textContent = nitbgi2r;
+let nitbli1r="LazyComet"; let nitbli1t="1:46.06"; let nitbli1a="https://www.twitch.tv/videos/1441988245";	document.getElementById("nitbli1l).textContent = nitbli1t;document.getElementById("nitbli1).textContent = nitbli1r;
+let nitblp1r="LazyComet"; let nitblp1t="1:54.78"; let nitblp1a="https://www.twitch.tv/videos/1445009092";	document.getElementById("nitblp1l).textContent = nitblp1t;document.getElementById("nitblp1).textContent = nitblp1r;
+let nitbna1r="LazyComet"; let nitbna1t="3:15.76"; let nitbna1a="https://www.twitch.tv/videos/1441988246";	document.getElementById("nitbna1l).textContent = nitbna1t;document.getElementById("nitbna1).textContent = nitbna1r;
+let nitbny1r="LazyComet"; let nitbny1t="1:48.75"; let nitbny1a="https://www.twitch.tv/videos/1445009093";	document.getElementById("nitbny1l).textContent = nitbny1t;document.getElementById("nitbny1).textContent = nitbny1r;
+let nitbsg1r="LazyComet"; let nitbsg1t="1:41.96"; let nitbsg1a="https://www.twitch.tv/videos/1445009094";	document.getElementById("nitbsg1l).textContent = nitbsg1t;document.getElementById("nitbsg1).textContent = nitbsg1r;
+let nitbvc1r="LazyComet"; let nitbvc1t="1:30.99"; let nitbvc1a="https://www.twitch.tv/videos/1445009090";	document.getElementById("nitbvc1l).textContent = nitbvc1t;document.getElementById("nitbvc1).textContent = nitbvc1r;    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     document.getElementById("nibftp1").classList.add('AxlThunder');
     document.getElementById("nibfli1").classList.add('AxlThunder');
@@ -37,12 +51,5 @@ let nitbna1r	="LazyComet";	let nitbna1t	="3:15.76";	let nitbna1a	="https://www.t
 //                          cell link id                  track time                       cell ID           track runner
 //  document.getElementById("nidttp1l").textContent = nidttp1t; document.getElementById("nidttp1").classList.add(nidttp1r);
 
- // document.getElementById("nibfna1l").textContent = nibfna1t; document.getElementById("nibfna1").classList.add(nibfna1r);
-  document.getElementById("nirhac1l").textContent = nirhac1t; document.getElementById("nirhac1").classList.add(nirhac1r);
-  document.getElementById("nirhfe1l").textContent = nirhfe1t; document.getElementById("nirhfe1").classList.add(nirhfe1r);
-  document.getElementById("nitbac1l").textContent = nitbac1t; document.getElementById("nitbac1").classList.add(nitbac1r);
-  document.getElementById("nitbfe1l").textContent = nitbfe1t; document.getElementById("nitbfe1").classList.add(nitbfe1r);
-  document.getElementById("nitbgi1l").textContent = nitbgi1t; document.getElementById("nitbgi1").classList.add(nitbgi1r);
-  document.getElementById("nitbli1l").textContent = nitbli1t; document.getElementById("nitbli1").classList.add(nitbli1r);
-  document.getElementById("nitbna1l").textContent = nitbna1t; document.getElementById("nitbna1").classList.add(nitbna1r);
+
 }
