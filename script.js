20af0339fee38ca2a94dc7149ce8d4fe3c866f2e
JBFcrasher <82116224+JBFcrasher@users.noreply.github.com>
2022-04-02 17:24:37 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 function applycss(){
     let nidttp1r ="JBFcrasher"; let nidttp1t="1:23.45"; 
     let nirhac1r="Gamedraco"; let nirhac1t="1:33.79";
-    //let nirhfe1r="Gamedraco", let nirhfe1t="1:25.43"; let nirhfe1l="https://www.youtube.com/watch?v=OKjYxLI12ak";
+    let nirhfe1r="Gamedraco", let nirhfe1t="1:25.43"; let nirhfe1l="https://www.youtube.com/watch?v=OKjYxLI12ak";
     
     document.getElementById("nibftp1").classList.add('AxlThunder');
     document.getElementById("nibfli1").classList.add('AxlThunder');
@@ -41,6 +41,7 @@ function applycss(){
     //document.getElementById("nidttp1").classList.add('JBFcrasher'); document.GetElementById("nidttp1l").innerHTML = "1:12.29";
   document.getElementById("nidttp1l").textContent = nidttp1t; document.getElementById("nidttp1").classList.add(nidttp1r);
   document.getElementById("nirhac1l").textContent = nirhac1t; document.getElementById("nirhac1").classList.add(nirhac1r);
+  document.getElementById("nirhfe1l").textContent = nirhfe1t; document.getElementById("nirhfe1").classList.add(nirhfe1r);
 }
 
 
