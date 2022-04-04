cca68b274ac9318c266961fc1e03b835e70cab2f
JBFcrasher <82116224+JBFcrasher@users.noreply.github.com>
2022-04-04 12:26:40 -0700

    Update script.js

--- script.js
+++ script.js
@@ -30,7 +30,8 @@ let psrhac1r="Gamedraco"; let psrhac1t="1:36.42"; let psrhac1a="https://www.yout
 let psrhgi1r="Gamedraco"; let psrhgi1t="1:31.18"; let psrhgi1a="https://www.youtube.com/watch?v=koiaHQzfUGw";    
     
 
-var myElementnitbli1p = document.getElementById("nitbli1p");if(myElementnitbli1p){ document.getElementById("nitbli1d").textContent = nitbli1t;document.getElementById("nitbli1p").classList.add(nitbli1r);}    
+var myElementnitbli1p = document.getElementById("nitbli1p");if(myElementnitbli1p){ document.getElementById("nitbli1d").textContent = nitbli1t;document.getElementById("nitbli1p").classList.add(nitbli1r);
+var a = document.getElementById('nitbli1d');a.href = nitbli1a}    
     
     
     
