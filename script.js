f8a4eb1e07e11ebedbd8a437a8a93d5bd8891c03
JBFcrasher <82116224+JBFcrasher@users.noreply.github.com>
2022-04-03 19:49:35 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,21 +2,32 @@
 
 
 function applycss(){
-let nibfna2r="Gamedraco";let nibfna2t="2:44.40";let nibfna2a="https://www.youtube.com/watch?v=xeFMb4eP05Y&t=1s";
-let nirhac1r="Gamedraco";let nirhac1t="1:33.79";let nirhac1a="https://www.youtube.com/watch?v=nOOIJa7WsMo";
-let nirhfe1r="Gamedraco";let nirhfe1t="1:25.43";let nirhfe1a="https://www.youtube.com/watch?v=OKjYxLI12ak";
-let nitbac1r="LazyComet";let nitbac1t="1:50.32";let nitbac1a="https://www.twitch.tv/videos/1441988243";
-//let nitbfe1r	="LazyComet";	let nitbfe1t	="1:45.79";	let nitbfe1a	="https://www.twitch.tv/videos/1441988249";
-//let nitbgi1r	="LazyComet";	let nitbgi1t	="1:45.75";	let nitbgi1a	="https://www.twitch.tv/videos/1441988244";
-let nitbli1r="LazyComet";let nitbli1t="1:46.06";let nitbli1a="https://www.twitch.tv/videos/1441988245";
-let nitbna1r="LazyComet";let nitbna1t="3:15.76";let nitbna1a="https://www.twitch.tv/videos/1441988246";
+let dcbfgi1r="AxlThunder"; let dcbfgi1t="1:21.52"; let dcbfgi1a="https://www.youtube.com/watch?v=0qVxl6PueuQ";
+let dcbfli1r="AxlThunder"; let dcbfli1t="1:24.08"; let dcbfli1a="https://www.youtube.com/watch?v=8xgGGFmWdzQ";
+let dcbfsg1r="AxlThunder"; let dcbfsg1t="1:26.75"; let dcbfsg1a="https://www.youtube.com/watch?v=UuzAz5GfNog";
+let dcrhgi1r="AxlThunder"; let dcrhgi1t="1:26.25"; let dcrhgi1a="https://www.youtube.com/watch?v=B2_evQM4d88";
+let nibfna1r="Gamedraco"; let nibfna1t="2:44.40"; let nibfna1a="https://www.youtube.com/watch?v=xeFMb4eP05Y&t=1s";
+let nirhac1r="Gamedraco"; let nirhac1t="1:33.79"; let nirhac1a="https://www.youtube.com/watch?v=nOOIJa7WsMo";
+let nirhfe1r="Gamedraco"; let nirhfe1t="1:25.43"; let nirhfe1a="https://www.youtube.com/watch?v=OKjYxLI12ak";
+let nitbac1r="LazyComet"; let nitbac1t="1:50.32"; let nitbac1a="https://www.twitch.tv/videos/1441988243";
 let nitbfe1r="LazyComet"; let nitbfe1t="1:39.82"; let nitbfe1a="https://www.twitch.tv/videos/1445009096";
-let nitbgi1r="LazyComet"; let nitbgi1t="1:41.85"; let nitbgi1a="https://www.twitch.tv/videos/1445009095";    
-let nitblp1r="LazyComet"; let nitblp1t="1:54.78"; let nitblp1a="https://www.twitch.tv/videos/1445009092"; 
-let nitbny1r="LazyComet"; let nitbny1t="1:48.75"; let nitbny1a="https://www.twitch.tv/videos/1445009093";    
-let nitbsg1r="LazyComet"; let nitbsg1t="1:41.96"; let nitbsg1a="https://www.twitch.tv/videos/1445009094";    
-let nitbvc1r="LazyComet"; let nitbvc1t="1:30.99"; let nitbvc1a="https://www.twitch.tv/videos/1445009090";    
-    
+let nitbgi1r="LazyComet"; let nitbgi1t="1:41.85"; let nitbgi1a="https://www.twitch.tv/videos/1445009095";
+let nitbli1r="LazyComet"; let nitbli1t="1:46.06"; let nitbli1a="https://www.twitch.tv/videos/1441988245";
+let nitblp1r="LazyComet"; let nitblp1t="1:54.78"; let nitblp1a="https://www.twitch.tv/videos/1445009092";
+let nitbna1r="LazyComet"; let nitbna1t="3:15.76"; let nitbna1a="https://www.twitch.tv/videos/1441988246";
+let nitbny1r="LazyComet"; let nitbny1t="1:48.75"; let nitbny1a="https://www.twitch.tv/videos/1445009093";
+let nitbsg1r="LazyComet"; let nitbsg1t="1:41.96"; let nitbsg1a="https://www.twitch.tv/videos/1445009094";
+let nitbvc1r="LazyComet"; let nitbvc1t="1:30.99"; let nitbvc1a="https://www.twitch.tv/videos/1445009090";
+let pcarli1r="AxlThunder"; let pcarli1t="1:36.86"; let pcarli1a="https://www.youtube.com/watch?v=iHgbVQKXUWo";
+let pcarvc1r="WeenieWhistle"; let pcarvc1t="1:19.43"; let pcarvc1a="https://youtu.be/rl6L6onWTyo";
+let pcarvc2r="AxlThunder"; let pcarvc2t="1:19.89"; let pcarvc2a="https://www.youtube.com/watch?v=Sl1HWo1ZihE";
+let pcctgi1r="AxlThunder"; let pcctgi1t="1:17.39"; let pcctgi1a="https://www.youtube.com/watch?v=SIO4W85xzmg";
+let pcrbli1r="AxlThunder"; let pcrbli1t="1:31.89"; let pcrbli1a="https://www.youtube.com/watch?v=V1flcTZ8TCw";
+let pcrhli1r="AxlThunder"; let pcrhli1t="1:26.86"; let pcrhli1a="https://www.youtube.com/watch?v=M5Fn4Lfc4Eo";
+let pctbli1r="AxlThunder"; let pctbli1t="1:33.32"; let pctbli1a="https://www.youtube.com/watch?v=EpnQNmZM6xU";
+let pctblp1r="AxlThunder"; let pctblp1t="1:38.79"; let pctblp1a="https://www.youtube.com/watch?v=sTNq-KkcWXo";
+let psrhac1r="Gamedraco"; let psrhac1t="1:36.42"; let psrhac1a="https://www.youtube.com/watch?v=zGz7anGg2co";
+let psrhgi1r="Gamedraco"; let psrhgi1t="1:31.18"; let psrhgi1a="https://www.youtube.com/watch?v=koiaHQzfUGw";    
     
 
     
@@ -26,33 +37,32 @@ let nitbvc1r="LazyComet"; let nitbvc1t="1:30.99"; let nitbvc1a="https://www.twit
     
     
     
-var myElement = document.getElementById("nirhac1");if(myElement){ document.getElementById("dcbfgi1l").textContent = dcbfgi1t;document.getElementById("dcbfgi1").classList.add(dcbfgi1r);}
-var myElement = document.getElementById("nirhac1");if(myElement){ document.getElementById("dcbfli1l").textContent = dcbfli1t;document.getElementById("dcbfli1").classList.add(dcbfli1r);}
-var myElement = document.getElementById("nirhac1");if(myElement){ document.getElementById("dcbfsg1l").textContent = dcbfsg1t;document.getElementById("dcbfsg1").classList.add(dcbfsg1r);}
-var myElement = document.getElementById("nirhac1");if(myElement){ document.getElementById("dcrhgi1l").textContent = dcrhgi1t;document.getElementById("dcrhgi1").classList.add(dcrhgi1r);}
-var myElement = document.getElementById("nirhac1");if(myElement){ document.getElementById("nibfna1l").textContent = nibfna1t;document.getElementById("nibfna1").classList.add(nibfna1r);}
+var myElement = document.getElementById("dcbfgi1");if(myElement){ document.getElementById("dcbfgi1l").textContent = dcbfgi1t;document.getElementById("dcbfgi1").classList.add(dcbfgi1r);}
+var myElement = document.getElementById("dcbfli1");if(myElement){ document.getElementById("dcbfli1l").textContent = dcbfli1t;document.getElementById("dcbfli1").classList.add(dcbfli1r);}
+var myElement = document.getElementById("dcbfsg1");if(myElement){ document.getElementById("dcbfsg1l").textContent = dcbfsg1t;document.getElementById("dcbfsg1").classList.add(dcbfsg1r);}
+var myElement = document.getElementById("dcrhgi1");if(myElement){ document.getElementById("dcrhgi1l").textContent = dcrhgi1t;document.getElementById("dcrhgi1").classList.add(dcrhgi1r);}
+var myElement = document.getElementById("nibfna1");if(myElement){ document.getElementById("nibfna1l").textContent = nibfna1t;document.getElementById("nibfna1").classList.add(nibfna1r);}
 var myElement = document.getElementById("nirhac1");if(myElement){ document.getElementById("nirhac1l").textContent = nirhac1t;document.getElementById("nirhac1").classList.add(nirhac1r);}
-var myElement = document.getElementById("nirhac1");if(myElement){ document.getElementById("nirhfe1l").textContent = nirhfe1t;document.getElementById("nirhfe1").classList.add(nirhfe1r);}
-var myElement = document.getElementById("nirhac1");if(myElement){ document.getElementById("nitbac1l").textContent = nitbac1t;document.getElementById("nitbac1").classList.add(nitbac1r);}
-var myElement = document.getElementById("nirhac1");if(myElement){ document.getElementById("nitbfe1l").textContent = nitbfe1t;document.getElementById("nitbfe1").classList.add(nitbfe1r);}
-var myElement = document.getElementById("nirhac1");if(myElement){ document.getElementById("nitbgi1l").textContent = nitbgi1t;document.getElementById("nitbgi1").classList.add(nitbgi1r);}
-var myElement = document.getElementById("nirhac1");if(myElement){ document.getElementById("nitbli1l").textContent = nitbli1t;document.getElementById("nitbli1").classList.add(nitbli1r);}
-var myElement = document.getElementById("nirhac1");if(myElement){ document.getElementById("nitblp1l").textContent = nitblp1t;document.getElementById("nitblp1").classList.add(nitblp1r);}
-var myElement = document.getElementById("nirhac1");if(myElement){ document.getElementById("nitbna1l").textContent = nitbna1t;document.getElementById("nitbna1").classList.add(nitbna1r);}
-var myElement = document.getElementById("nirhac1");if(myElement){ document.getElementById("nitbny1l").textContent = nitbny1t;document.getElementById("nitbny1").classList.add(nitbny1r);}
-var myElement = document.getElementById("nirhac1");if(myElement){ document.getElementById("nitbsg1l").textContent = nitbsg1t;document.getElementById("nitbsg1").classList.add(nitbsg1r);}
-var myElement = document.getElementById("nirhac1");if(myElement){ document.getElementById("nitbvc1l").textContent = nitbvc1t;document.getElementById("nitbvc1").classList.add(nitbvc1r);}
-var myElement = document.getElementById("nirhac1");if(myElement){ document.getElementById("pcarli1l").textContent = pcarli1t;document.getElementById("pcarli1").classList.add(pcarli1r);}
-var myElement = document.getElementById("nirhac1");if(myElement){ document.getElementById("pcarvc1l").textContent = pcarvc1t;document.getElementById("pcarvc1").classList.add(pcarvc1r);}
-var myElement = document.getElementById("nirhac1");if(myElement){ document.getElementById("pcarvc2l").textContent = pcarvc2t;document.getElementById("pcarvc2").classList.add(pcarvc2r);}
-var myElement = document.getElementById("nirhac1");if(myElement){ document.getElementById("pcctgi1l").textContent = pcctgi1t;document.getElementById("pcctgi1").classList.add(pcctgi1r);}
-var myElement = document.getElementById("nirhac1");if(myElement){ document.getElementById("pcrbli1l").textContent = pcrbli1t;document.getElementById("pcrbli1").classList.add(pcrbli1r);}
-var myElement = document.getElementById("nirhac1");if(myElement){ document.getElementById("pcrhli1l").textContent = pcrhli1t;document.getElementById("pcrhli1").classList.add(pcrhli1r);}
-var myElement = document.getElementById("nirhac1");if(myElement){ document.getElementById("pctbli1l").textContent = pctbli1t;document.getElementById("pctbli1").classList.add(pctbli1r);}
-var myElement = document.getElementById("nirhac1");if(myElement){ document.getElementById("pctblp1l").textContent = pctblp1t;document.getElementById("pctblp1").classList.add(pctblp1r);}
-var myElement = document.getElementById("nirhac1");if(myElement){ document.getElementById("psrhac1l").textContent = psrhac1t;document.getElementById("psrhac1").classList.add(psrhac1r);}
-var myElement = document.getElementById("nirhac1");if(myElement){ document.getElementById("psrhgi1l").textContent = psrhgi1t;document.getElementById("psrhgi1").classList.add(psrhgi1r);}    
-    
+var myElement = document.getElementById("nirhfe1");if(myElement){ document.getElementById("nirhfe1l").textContent = nirhfe1t;document.getElementById("nirhfe1").classList.add(nirhfe1r);}
+var myElement = document.getElementById("nitbac1");if(myElement){ document.getElementById("nitbac1l").textContent = nitbac1t;document.getElementById("nitbac1").classList.add(nitbac1r);}
+var myElement = document.getElementById("nitbfe1");if(myElement){ document.getElementById("nitbfe1l").textContent = nitbfe1t;document.getElementById("nitbfe1").classList.add(nitbfe1r);}
+var myElement = document.getElementById("nitbgi1");if(myElement){ document.getElementById("nitbgi1l").textContent = nitbgi1t;document.getElementById("nitbgi1").classList.add(nitbgi1r);}
+var myElement = document.getElementById("nitbli1");if(myElement){ document.getElementById("nitbli1l").textContent = nitbli1t;document.getElementById("nitbli1").classList.add(nitbli1r);}
+var myElement = document.getElementById("nitblp1");if(myElement){ document.getElementById("nitblp1l").textContent = nitblp1t;document.getElementById("nitblp1").classList.add(nitblp1r);}
+var myElement = document.getElementById("nitbna1");if(myElement){ document.getElementById("nitbna1l").textContent = nitbna1t;document.getElementById("nitbna1").classList.add(nitbna1r);}
+var myElement = document.getElementById("nitbny1");if(myElement){ document.getElementById("nitbny1l").textContent = nitbny1t;document.getElementById("nitbny1").classList.add(nitbny1r);}
+var myElement = document.getElementById("nitbsg1");if(myElement){ document.getElementById("nitbsg1l").textContent = nitbsg1t;document.getElementById("nitbsg1").classList.add(nitbsg1r);}
+var myElement = document.getElementById("nitbvc1");if(myElement){ document.getElementById("nitbvc1l").textContent = nitbvc1t;document.getElementById("nitbvc1").classList.add(nitbvc1r);}
+var myElement = document.getElementById("pcarli1");if(myElement){ document.getElementById("pcarli1l").textContent = pcarli1t;document.getElementById("pcarli1").classList.add(pcarli1r);}
+var myElement = document.getElementById("pcarvc1");if(myElement){ document.getElementById("pcarvc1l").textContent = pcarvc1t;document.getElementById("pcarvc1").classList.add(pcarvc1r);}
+var myElement = document.getElementById("pcarvc2");if(myElement){ document.getElementById("pcarvc2l").textContent = pcarvc2t;document.getElementById("pcarvc2").classList.add(pcarvc2r);}
+var myElement = document.getElementById("pcctgi1");if(myElement){ document.getElementById("pcctgi1l").textContent = pcctgi1t;document.getElementById("pcctgi1").classList.add(pcctgi1r);}
+var myElement = document.getElementById("pcrbli1");if(myElement){ document.getElementById("pcrbli1l").textContent = pcrbli1t;document.getElementById("pcrbli1").classList.add(pcrbli1r);}
+var myElement = document.getElementById("pcrhli1");if(myElement){ document.getElementById("pcrhli1l").textContent = pcrhli1t;document.getElementById("pcrhli1").classList.add(pcrhli1r);}
+var myElement = document.getElementById("pctbli1");if(myElement){ document.getElementById("pctbli1l").textContent = pctbli1t;document.getElementById("pctbli1").classList.add(pctbli1r);}
+var myElement = document.getElementById("pctblp1");if(myElement){ document.getElementById("pctblp1l").textContent = pctblp1t;document.getElementById("pctblp1").classList.add(pctblp1r);}
+var myElement = document.getElementById("psrhac1");if(myElement){ document.getElementById("psrhac1l").textContent = psrhac1t;document.getElementById("psrhac1").classList.add(psrhac1r);}
+var myElement = document.getElementById("psrhgi1");if(myElement){ document.getElementById("psrhgi1l").textContent = psrhgi1t;document.getElementById("psrhgi1").classList.add(psrhgi1r);}    
     
     
     
